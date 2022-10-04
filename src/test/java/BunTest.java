66337a5710eace04d224221997a3d400a71be309
UsikB <ni.github@gmail.com>
2022-10-04 17:47:56 +0300

    first commit

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BunTest extends BaseTest{
+
+    @Test
+    public void bunGetNameMethodReturnsName(){
+        bun = new Bun("testName", 12);
+        assertTrue(bun.getName().equals("testName"));
+    }
+
+    @Test
+    public void bunGetNameMethodReturnsPrice(){
+        bun = new Bun("testName", 12);
+        assertEquals(bun.getPrice(), 12, 0);
+    }
+}
