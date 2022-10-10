df610b28d09a198759f80b6a703232bafb5ed855
UsikB <ni.github@gmail.com>
2022-10-10 15:56:28 +0300

    for review2

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -7,15 +7,15 @@ import static org.junit.Assert.assertTrue;
 public class BunTest extends BaseTest{
 
 
-    @Test
-    public void bunGetNameMethodReturnsName(){
-        bun = new Bun("testName", 12);
-        assertTrue(bun.getName().equals("testName"));
-    }
+     @Test
+     public void bunGetNameMethodReturnsName(){
+         bun = new Bun("testName", 12);
+         assertTrue(bun.getName().equals("testName"));
+     }
 
-    @Test
-    public void bunGetNameMethodReturnsPrice(){
-        bun = new Bun("testName", 12);
-        assertEquals(bun.getPrice(), 12, 0);
-    }
+     @Test
+     public void bunGetNameMethodReturnsPrice(){
+         bun = new Bun("testName", 12);
+         assertEquals(bun.getPrice(), 12, 0);
+     }
 }
